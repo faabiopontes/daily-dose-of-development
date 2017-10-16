ab4fa7a5536d5f3cc0c07d73f775787581dbb33d
Fabio Pontes <faabiopontes@gmail.com>
2017-10-16 13:08:35 -0200

    FEA Start Object Literal Pattern - End Video Modular Javascript #1 - We have started explanations on how the Object Literal Pattern Module works

--- resources/assets/js/modularJS.js
+++ resources/assets/js/modularJS.js
@@ -1,21 +1,37 @@
-var people = [];
-var template = $('#people-template').html();
+// var people = [];
+// var template = $('#people-template').html();
 
-$('#peopleModule').find('button').on('click', function() {
-    people.push($('#peopleModule').find('input').val());
-    $('#peopleModule').find('input').val('');
-    //data for mustache template
-    var data = {
-        people: people,
-    };
-    $('#peopleModule').find('ul').html(Mustache.render(template, data));
-});
+// $('#peopleModule').find('button').on('click', function() {
+//     people.push($('#peopleModule').find('input').val());
+//     $('#peopleModule').find('input').val('');
+//     //data for mustache template
+//     var data = {
+//         people: people,
+//     };
+//     $('#peopleModule').find('ul').html(Mustache.render(template, data));
+// });
 
-$('#peopleModule').find('ul').delegate('i.del', 'click', function(e) {
-    var $remove = $(e.target).closest('li');
-    var i = $('#peopleModule').find('ul').find('li').index($remove);
+// $('#peopleModule').find('ul').delegate('i.del', 'click', function(e) {
+//     var $remove = $(e.target).closest('li');
+//     var i = $('#peopleModule').find('ul').find('li').index($remove);
 
-    $remove.remove();
+//     $remove.remove();
 
-    people.splice(i, 1);
-});
+//     people.splice(i, 1);
+// });
+
+
+//object literal
+var myModule = {
+   name: 'Fabio',
+   age: 25,
+   sayName: function() {
+       alert(this.name);
+    },
+    setName: function(newName) {
+        this.name = newName;
+    }
+};
+
+myModule.setName("faabiopontes");
+myModule.sayName();
