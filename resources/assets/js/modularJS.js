e35542edf08f7383039cc446a61f8d160c1e78b8
Fabio Pontes <faabiopontes@localhost.localdomain>
2017-10-24 01:20:03 -0200

    FEA Module Pattern Tries - We are watching the videos and trying stuff - Using a object we kind of have private, but via hacking (Sources tab) is still acessible

--- resources/assets/js/modularJS.js
+++ resources/assets/js/modularJS.js
@@ -1,39 +1,66 @@
-var people = {
-  people: ["Fabio", "Pontes"],
-  init: function () {
-    this.cacheDom();
-    this.bindEvents();
-    this.render();
-  },
-  cacheDom: function () {
-    this.$el = $('#peopleModule');
-    this.$button = this.$el.find('button');
-    this.$input = this.$el.find('input');
-    this.$ul = this.$el.find('ul');
-    this.template = this.$el.find('#people-template').html();
-  },
-  render: function () {
-    var data = {
-      people: this.people,
-    };
-    this.$ul.html(Mustache.render(this.template, data));
-  },
-  bindEvents: function () {
-    this.$button.on('click', this.addPerson.bind(this));
-    this.$ul.delegate('i.del', 'click', this.deletePerson.bind(this));
-  },
-  addPerson: function (eventOrName) {
-    var name = typeof eventOrName == "string" ? eventOrName : this.$input.val();
-    this.people.push(name);
-    this.$input.val('');
-    this.render();
-  },
-  deletePerson: function (event) {
-    var $remove = $(event.target).closest('li');
-    var i = this.$ul.find('li').index($remove);
+// trying to be private
+// but still public hacking Sources of navigator
+var peopleModule = (function () {
+  var peopleObject = {
+    names: ["Fabio", "Pontes"],
+    init: function () {
+      this.cacheDom();
+      this.bindEvents();
+      this.render();
+      this.returnFunctions();
+    },
+    returnFunctions: function () {
+      return {
+        addPerson: this.addPerson,
+        render: this.render
+      }
+    },
+    cacheDom: function () {
+      this.$el = $('#peopleModule');
+      this.$button = this.$el.find('button');
+      this.$input = this.$el.find('input');
+      this.$ul = this.$el.find('ul');
+      this.template = this.$el.find('#people-template').html();
+    },
+    render: function () {
+      var data = {
+        people: this.names,
+      };
+      this.$ul.html(Mustache.render(this.template, data));
+    },
+    bindEvents: function () {
+      this.$button.on('click', this.addPerson.bind(this));
+      this.$ul.delegate('i.del', 'click', this.deletePerson.bind(this));
+    },
+    addPerson: function (eventOrName) {
+      var name = typeof eventOrName == "string" ? eventOrName : this.$input.val();
+      this.names.push(name);
+      this.$input.val('');
+      this.render();
+    },
+    deletePerson: function (event) {
+      var $remove = $(event.target).closest('li');
+      var i = this.$ul.find('li').index($remove);
 
-    this.people.splice(i, 1);
-    this.render();
+      this.names.splice(i, 1);
+      this.render();
+    }
+  };
+  peopleObject.init();
+})();
+// people.init();
+
+var peoplePrivate = (function () {
+  // this is function scope
+
+  // this is a private variable
+  var privateName = 'pontes';
+
+  function sayPrivateName() {
+    alert(privateName);
   }
-};
-people.init();
+  return {
+    sayPrivateName: sayPrivateName
+  }
+})();
+// peoplePrivate.sayPrivateName();
