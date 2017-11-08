9ccbd617d948d7e2a6452642b19f6862ccede3d3
Fabio Pontes <faabiopontes@gmail.com>
2017-11-08 12:04:53 -0200

    FEA Pusher HelloWorld View

---
+++ resources/views/pusher.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<head>
+  <title>Pusher Test</title>
+  <script src="https://js.pusher.com/4.1/pusher.min.js"></script>
+  <script>
+
+    // Enable pusher logging - don't include this in production
+    Pusher.logToConsole = true;
+
+    var pusher = new Pusher('9f9d8b9266b0580d23dd', {
+      cluster: 'us2',
+      encrypted: true
+    });
+
+    var channel = pusher.subscribe('my-channel');
+    channel.bind('my-event', function(data) {
+      alert(data.message);
+    });
+  </script>
+</head>
