795cfee071d3ed2c4b9c4b332dc822270fd35eae
Fabio Pontes <faabiopontes@gmail.com>
2017-11-13 14:54:51 -0200

    FEA Firebase App - Lesson 185 - Create project on https://console.firebase.google.com/ - Added config code for Web App - Added firebase package on NPM

--- ReactRipsOfRelease/App.js
+++ ReactRipsOfRelease/App.js
@@ -1,23 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'firebase';
+import React, { Component } from 'react';
+import { AppRegistry, View, Teext, Button } from 'react-native';
 
-export default class App extends React.Component {
+class App extends Component {
+
+  componentWillMount() {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyA-omyitJmqWdZaLw7Zt5Ta1brULZhrgFY",
+      authDomain: "fir-udemy-59f38.firebaseapp.com",
+      databaseURL: "https://fir-udemy-59f38.firebaseio.com",
+      projectId: "fir-udemy-59f38",
+      storageBucket: "fir-udemy-59f38.appspot.com",
+      messagingSenderId: "495930199170"
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View>
+        <Text>My App</Text>
       </View>
     );
   }
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+AppRegistry.registerComponent('firebaseTest', () => App);
