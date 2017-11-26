9fa22ce891c0b5d6474e7190b3084fe13f432371
Fabio Pontes <faabiopontes@gmail.com>
2017-11-26 02:11:07 -0200

    FEA Ending Modular Javascript #5 - PubSub Implementation - The people module now emits a event of peopleChanged, this event is called without knowing if there is going to be a listener. If there is no listeners thats ok - The stats module counts and shows the indexed number of people. It is now completely decoupled of the peopleModule. We can use stats.destroy() to remove the stats functionality and our people module will still work. - We fixed the JS files to be equal to the ones in the tutorial - Changed the order of JS files on webpack.mix.js so the order is now pubsub, stats, people. Because stats and people depends on pubsub for the events object - We added sourceMaps but it is now working as intended.. - The people and pubsub files had some garbage from the first parts and tests of mine, now removed

--- resources/assets/js/modularJS/pubsub.js
+++ resources/assets/js/modularJS/pubsub.js
@@ -1,32 +1,25 @@
+//events (publish subscribe) pattern
 var events = {
   events: {},
   on: function (eventName, fn) {
     this.events[eventName] = this.events[eventName] || [];
     this.events[eventName].push(fn);
   },
-  off: function (eventName, fn) {
+  off: function(eventName, fn) {
     if (this.events[eventName]) {
-      for (i = 0; i < this.events[eventName].length; i++) {
+      for (var i = 0; i < this.events[eventName].length; i++) {
         if (this.events[eventName][i] === fn) {
           this.events[eventName].splice(i, 1);
           break;
         }
-      }
+      };
     }
   },
   emit: function (eventName, data) {
     if (this.events[eventName]) {
-      this.events[eventName].forEach(function (fn) {
+      this.events[eventName].forEach(function(fn) {
         fn(data);
       });
     }
   }
-}
-
-// Example creating a event
-events.on("consoleParameter", function(parameter) {
-  console.log(parameter);
-});
-
-// Example of emitting a event created
-events.emit("consoleParameter", "PubSubPattern Rocks");
+};
--- webpack.mix.js
+++ webpack.mix.js
@@ -15,14 +15,15 @@ mix.js('resources/assets/js/app.js', 'public/js')
   .scripts([
     'node_modules/jquery/dist/jquery.min.js',
     'node_modules/mustache/mustache.min.js',
-    'resources/assets/js/modularJS/people.js',
     'resources/assets/js/modularJS/pubsub.js',
     'resources/assets/js/modularJS/stats.js',
+    'resources/assets/js/modularJS/people.js',
   ], 'public/js/modularJS.js')
   .scripts([
     'node_modules/pusher-js/dist/web/pusher.min.js',
     'resources/assets/js/pusher/script.js',
   ], 'public/js/pusher.js')
+  .sourceMaps()
   .styles('resources/assets/css/modularJS.css', 'public/css/modularJS.css')
   .scripts('resources/assets/js/appTinyMCE.js', 'public/js/appTinyMCE.js')
   .copyDirectory('node_modules/tinymce', 'public/plugins/tinymce')
