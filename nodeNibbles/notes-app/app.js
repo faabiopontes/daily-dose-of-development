54260e7ba780bfd0344efba88196cb31ea0e5ff5
faabiopontes <faabiopontes@gmail.com>
2018-01-04 02:16:29 -0200

    FEA Using 3rd Party Modules - Section 3, Lecture 11 - When we run "npm install lodash" it automatically goes to package.json without the --save flag - .gitignore added for node_modules - Rewritten explanation about require

--- nodeNibbles/notes-app/app.js
+++ nodeNibbles/notes-app/app.js
@@ -1,11 +1,18 @@
 console.log('Starting app.js');
 const fs = require('fs');
 const os = require('os');
-// Require is used to get built-in modules
-// All modules available are on the API
-
+const _ = require('lodash');
 const notes = require('./notes.js');
-// We can also require our own files
+// Require is used to get modules from the outside
+// There are there kinds of modules we can use
+// Built-in modules: All modules available are on the Node API
+// Our own modules: Files or modules created by us, like notes.js
+// Third-party modules: Like lodash or anything available on NPM
+
+// console.log(_.isString(true));
+// console.log(_.isString('Fabio'));
+var filteredArray = _.uniq(['Fabio',1,'Pontes',1,2,3,4]);
+console.log(filteredArray);
 
 var res = notes.addNote();
 
