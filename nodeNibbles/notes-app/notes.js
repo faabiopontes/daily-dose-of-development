86dfa8f6e3c90744da4e5f87649dc5920b945b14
faabiopontes <faabiopontes@gmail.com>
2018-01-07 03:10:26 -0200

    Simplified Input With Yargs - Section 3, Lecture 14 - Installed NPM module Yargs - Yargs makes the process of parsing your arguments much easier, always returning the same format - The global variable process.argv is not used anymore to get parameters, now using only Yargs - In the notes.js file we are using ES6 in the function declaration with () => {}  and simplified object export when the atribute has the same name as the variable

--- nodeNibbles/notes-app/notes.js
+++ nodeNibbles/notes-app/notes.js
@@ -1,8 +1,24 @@
 console.log('Starting notes.js');
 
-module.exports.age = 25;
-module.exports.name = "Fabio";
-module.exports.addNote = () => {
-    console.log('addNote');
-    return 'New note';
+var addNote = (title, body) => {
+    console.log('Adding note', title, body);
+};
+var getAll = () => {
+    console.log('Getting all notes');
+};
+var getNote = (title) => {
+    console.log('Getting note',title);
 }
+var removeNote = (title) => {
+    console.log('Removing note',title);
+}
+
+module.exports = {
+    addNote,
+    // this is the same as
+    // addNote: addNote
+    // since the property is the same name as the variable defined
+    getAll,
+    getNote,
+    removeNote
+};
