4d3f4d2e79801c009fc396ae759252c13ec86451
Fabio Pontes <faabiopontes@gmail.com>
2018-01-08 14:48:19 -0200

    FEA Adding and Saving Notes - Section 3, Lecture 16 - The launch.json was modified so the arguments were started - Now we are adding and saving notes to a file - Checking if file already has a note with the same title too

--- nodeNibbles/notes-app/notes.js
+++ nodeNibbles/notes-app/notes.js
@@ -1,16 +1,44 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
 var addNote = (title, body) => {
-    console.log('Adding note', title, body);
+    var notes;
+    var note = {
+        title,
+        body
+    };
+
+    try {
+        var notesString = fs.readFileSync(__dirname + '/notes-data.json');
+        notes = JSON.parse(notesString);
+    } catch (e) {
+        notes = [];
+    }
+
+    var duplicateNotes = notes.filter((note) => note.title === title);
+    // When we are doing one verification
+    // The line above does the same as the lines below
+    // var duplicateNotes = notes.filter((note) => {
+    //     return note.title === title;
+    // });
+    if (duplicateNotes.length === 0) {
+        console.log('Adding note', title, body);
+        notes.push(note);
+        fs.writeFileSync(__dirname + '/notes-data.json', JSON.stringify(notes));
+    } else {
+        console.log('Note Title already exists!');
+    }
+
 };
 var getAll = () => {
     console.log('Getting all notes');
 };
 var getNote = (title) => {
-    console.log('Getting note',title);
+    console.log('Getting note', title);
 }
 var removeNote = (title) => {
-    console.log('Removing note',title);
+    console.log('Removing note', title);
 }
 
 module.exports = {
