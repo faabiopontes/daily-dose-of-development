92e9761da70f6153c162766faebccdb4176f1430
faabiopontes <faabiopontes@gmail.com>
2018-01-11 07:54:54 -0200

    FEA Route for Specific User on Express

--- nodeNibbles/express-quick-start/routes/users.js
+++ nodeNibbles/express-quick-start/routes/users.js
@@ -5,5 +5,11 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+/* GET specific user. */
+router.get('/:userId', function(req, res, next) {
+  console.log(req.params);
+  let text = 'respond with info from user: '+req.params.userId;
+  res.send(text);
+});
 
 module.exports = router;
