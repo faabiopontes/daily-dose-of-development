e89de5fcaeac8150ac77b725e5dfcf4122cbad3f
Fabio Pontes <faabiopontes@gmail.com>
2018-01-18 13:08:21 -0200

    FEA Callback Errors - Section 4, Lecture 32 - The code already showed some errors, but now the messages to the user is more complete

--- nodeNibbles/weather-app/app.js
+++ nodeNibbles/weather-app/app.js
@@ -20,10 +20,14 @@ request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
-  if (body.error_message) {
+  if (error) {
+    console.log('Unable to connect to Google servers.');
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log('Unable to find that address');
+  } else if (body.error_message) {
     console.log("Error Message: " + body.error_message);
     console.log("Status: " + body.error_message.status);
-  } else {
+  } else if (body.status === 'OK') {
     console.log(`Address: ${body.results[0].formatted_address}`);
     console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
     console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
