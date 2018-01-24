9b60713c8c1585875fabb8120acf86ba8539986f
Fabio Pontes <faabiopontes@gmail.com>
2018-01-24 14:11:12 -0200

    FEA Wiring Up Weather Search - Section 4, Lecture - We are now making requests to the - Created files with mock results from the requests to Google and Forecast - Created account at https://darksky.net/dev

--- nodeNibbles/weather-app/app.js
+++ nodeNibbles/weather-app/app.js
@@ -1,24 +1,47 @@
-const yargs = require('yargs');
+// const yargs = require('yargs');
 
-const geocode = require('./geocode/geocode.js');
+// const geocode = require('./geocode/geocode.js');
 
-const argv = yargs
-  .options({
-    address: {
-      demand: true,
-      alias: 'a',
-      describe: 'Address to fetch weather for',
-      string: true
-    }
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
+// const argv = yargs
+//   .options({
+//     address: {
+//       demand: true,
+//       alias: 'a',
+//       describe: 'Address to fetch weather for',
+//       string: true
+//     }
+//   })
+//   .help()
+//   .alias('help', 'h')
+//   .argv;
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  if(errorMessage) {
-    console.log(errorMessage);
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   if (errorMessage) {
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
+
+
+// Require Request
+// Make request to DarkSky API using
+// Show the temperature property to the screen
+const request = require('request');
+request({
+  url: `https://api.darksky.net/forecast/4eca276784134e501dbf3f8296d80890/-30.0192482,-51.1954874`,
+  json: true
+}, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    console.log(`Temperature: ${body.currently.temperature}`);
   } else {
-    console.log(JSON.stringify(results, undefined, 2));
+    console.log('Unable to fetch weather.');
   }
 });
+
+// Forecast Request
+// https://api.darksky.net/forecast/[key]/[latitude],[longitude]
+// https://api.darksky.net/forecast/4eca276784134e501dbf3f8296d80890/-30.0192482,-51.1954874
+
+// Forecast Key
+// 4eca276784134e501dbf3f8296d80890
