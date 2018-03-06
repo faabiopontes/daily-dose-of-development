c857a22b21b7b88f1f7a4b9e651fd7876f77a6b8
Fabio Pontes <faabiopontes@gmail.com>
2018-03-06 13:24:00 -0300

    FEA Rendering Templates with Data - Section 5, Lecture 43 - Installed Handlebars as view engine for our web server - Created and rendered templates with a object with data - Link NPM package: https://www.npmjs.com/package/hbs

--- nodeNibbles/web-server/server.js
+++ nodeNibbles/web-server/server.js
@@ -1,23 +1,33 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 var port = 3000;
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   // res.send('<h1>Hello Express!</h1>');
-  res.send({
-    name: 'Fabio',
-    likes: [
-      'Biking',
-      'Music'
-    ]
+  // reace this to a call to res.render
+  // brand new home.hbs
+  // inside we are going to have the same properties below
+  // plus welcome message
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'Learning Node with Andrew Mead'
   });
 });
 
 app.get('/about', (req, res) => {
-  res.send('<h1>About Page</h1>');
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
+
 });
 
 // /bad - send back json with errorMessage
@@ -28,6 +38,6 @@ app.get('/bad', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log('Server is up and running on port '+port);
+  console.log('Server is up and running on port ' + port);
 });
 console.log('Server is running');
