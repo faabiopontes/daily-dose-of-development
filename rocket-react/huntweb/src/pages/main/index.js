331e9b20bd0efd2c671bd8e15e51474d0964f7fb
Fabio Pontes <fabio.pontes@acasadoconcurseiro.com.br>
2018-12-12 14:42:50 -0200

    RocketSeat - React - 9: Configuring Navigation

--- rocket-react/huntweb/src/pages/main/index.js
+++ rocket-react/huntweb/src/pages/main/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 
@@ -29,7 +30,7 @@ export default class Main extends Component {
   };
 
   prevPage = () => {
-    const { page, productInfo } = this.state;
+    const { page } = this.state;
 
     if (page === 1) return;
 
@@ -57,7 +58,7 @@ export default class Main extends Component {
           <article key={product._id}>
             <strong>{product.title}</strong>
             <p>{product.description}</p>
-            <a href="">Acessar</a>
+            <Link to={`/products/${product._id}`}>Acessar</Link>
           </article>
         ))}
         <div className="actions">
