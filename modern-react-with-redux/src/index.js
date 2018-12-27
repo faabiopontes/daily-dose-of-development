4c44f7d9193f6f224ad0626c84416b6cbbc7674d
Fabio Pontes <fabio.pontes@acasadoconcurseiro.com.br>
2018-12-27 13:28:22 -0200

    FEA MRwR s1l10: Displaying Content with Functional Components - We wrote our basic HelloWorld to the screen with React - We don't actually need to extend Componet for it to work, I guess we only need it on stateful components - ReactDOM.render is called with two paremeters - The first parameter is the JSX Element, or React Component - The second parameter is the HTML element that it's the first parameter is going to bind

--- modern-react-with-redux/src/index.js
+++ modern-react-with-redux/src/index.js
@@ -3,7 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Create a React Component
-
-
+const App = () => <div>Hi there!</div>;
 
 // Take the React Component and show it on the screen
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
