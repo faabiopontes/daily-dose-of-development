698efe2d27d00dd76a24293ee3d925c1fafd706e
Fabio Pontes <faabiopontes@gmail.com>
2018-12-27 20:58:50 -0200

    FEA MRwR S2L14: Converting HTML to JSX - After coping and pasting a valid HTML inside our div, we errors - JSX has some differences from HTML, as we will see on the next lecture

--- modern-react-with-redux/src/index.js
+++ modern-react-with-redux/src/index.js
@@ -3,7 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Create a React Component
-const App = () => <div>Hi there!</div>;
+const App = () => (
+  <div>
+    <label class="label" for="name">Enter name:</label>
+    <input id="name" type="text" />
+    <button style="background-color: blue; color: white;">Submit</button>
+  </div>
+);
 // the above line is converted to the line below using the Babel preset react
 // they are essentially the same thing
 // const App = () => React.createElement("div", null, "Hi there!");
