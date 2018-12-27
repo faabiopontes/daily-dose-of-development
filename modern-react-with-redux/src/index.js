5b79088abb2c0f165f91a1d643877e2265f26b3f
Fabio Pontes <faabiopontes@gmail.com>
2018-12-26 23:30:46 -0200

    FEA MRwR s1l9: Javascript Module Systems - Removed all files from the src folder - Created our own index.js with the imports necessary

--- modern-react-with-redux/src/index.js
+++ modern-react-with-redux/src/index.js
@@ -1,12 +1,9 @@
+// Import the React and the ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create a React Component
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+// Take the React Component and show it on the screen
