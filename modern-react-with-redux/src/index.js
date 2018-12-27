1c1e9129b323d0d265ea9f99c03d27252e8df4d9
Fabio Pontes <faabiopontes@gmail.com>
2018-12-27 21:11:41 -0200

    FEA MRwR S2L17: Class vs ClassName - The atribute class is inteligent enough to understand either - There are discussions on the community about using each one

--- modern-react-with-redux/src/index.js
+++ modern-react-with-redux/src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 // Create a React Component
 const App = () => (
   <div>
-    <label class="label" for="name">Enter name:</label>
+    <label className="label" for="name">Enter name:</label>
     <input id="name" type="text" />
     <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
   </div>
