aa3e3fa3c4eb5379f49ed1609569ca0afa243953
Fabio Pontes <fabio.pontes@acasadoconcurseiro.com.br>
2018-12-27 13:45:24 -0200

    FEA MRwR s1l13: Whats is JSX? - No browser understands JSX, it gets converted to JS code - A <div> tag is converted to a function using React.createElement

--- modern-react-with-redux/src/index.js
+++ modern-react-with-redux/src/index.js
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom';
 
 // Create a React Component
 const App = () => <div>Hi there!</div>;
+// the above line is converted to the line below using the Babel preset react
+// they are essentially the same thing
+// const App = () => React.createElement("div", null, "Hi there!");
 
 // Take the React Component and show it on the screen
 ReactDOM.render(
