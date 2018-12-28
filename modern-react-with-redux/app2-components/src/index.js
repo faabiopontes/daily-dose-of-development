8bc10434d7581658cc0d4769ba30a36fa943fea9
Fabio Pontes <faabiopontes@gmail.com>
2018-12-28 18:07:11 -0200

    FEA MRwR S3L33: Passing Multiple Props

--- modern-react-with-redux/app2-components/src/index.js
+++ modern-react-with-redux/app2-components/src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 import CommentDetail from './CommentDetail';
 
 const App = () => (
   <div className="ui container comments">
-    <CommentDetail author="Sam" />
-    <CommentDetail author="Alex" />
-    <CommentDetail author="Jane" />
+    <CommentDetail
+      author="Sam"
+      timeAgo="Today at 4:45PM"
+      avatar={faker.image.avatar()}
+      content="Nice blog post!"
+    />
+    <CommentDetail
+      author="Alex"
+      timeAgo="Today at 2:00AM"
+      avatar={faker.image.avatar()}
+      content="Amazing blog post!"
+    />
+    <CommentDetail
+      author="Jane"
+      timeAgo="Today at 5:00PM"
+      avatar={faker.image.avatar()}
+      content="Awesome blog post!"
+    />
   </div>
 );
 
