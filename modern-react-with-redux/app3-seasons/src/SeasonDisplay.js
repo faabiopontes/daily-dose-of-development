061bf8bc0588f89e4546fcb4976a3de09abdf0c5
Fabio Pontes <fabio.pontes@acasadoconcurseiro.com.br>
2019-01-07 14:18:48 -0200

    FEA MRwR S6L62 Ternary Expressions in JSX - We could've used just a ternary in the render method, but isolating in a function we make the code more organized

--- modern-react-with-redux/app3-seasons/src/SeasonDisplay.js
+++ modern-react-with-redux/app3-seasons/src/SeasonDisplay.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
 const getSeason = (lat, month) => {
-  if(month > 2 && month < 9) {
+  if (month > 2 && month < 9) {
     return lat > 0 ? 'summer' : 'winter';
   } else {
     return lat > 0 ? 'winter' : 'summer';
   }
 }
 
+const getMessage = season => season === 'winter' ? 'Burr, it\' is chilly' : 'Let\'s hit the beach';
+
 const SeasonDisplay = props => {
   const season = getSeason(props.lat, new Date().getMonth());
+  const message = getMessage(season);
+
   console.log(season);
 
-  return <div>Season Display</div>;
+  return <div>{message}</div>;
 };
 
 export default SeasonDisplay;
