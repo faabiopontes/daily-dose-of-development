809a7675ded2fc3b69eba4efd8bfbf7af16e2b9d
Fabio Pontes <fabio.pontes@acasadoconcurseiro.com.br>
2019-01-07 14:40:58 -0200

    FEA MRwR S6L63: Showing Icons

--- modern-react-with-redux/app3-seasons/src/SeasonDisplay.js
+++ modern-react-with-redux/app3-seasons/src/SeasonDisplay.js
@@ -9,14 +9,20 @@ const getSeason = (lat, month) => {
 }
 
 const getMessage = season => season === 'winter' ? 'Burr, it\' is chilly' : 'Let\'s hit the beach';
+const getIcon = season => season === 'winter' ? 'snowflake' : 'sun';
 
 const SeasonDisplay = props => {
   const season = getSeason(props.lat, new Date().getMonth());
   const message = getMessage(season);
+  const icon = getIcon(season);
 
-  console.log(season);
-
-  return <div>{message}</div>;
+  return (
+    <div>
+      <i className={`icon ${icon}`} />
+      <h1>{message}</h1>
+      <i className={`icon ${icon}`} />
+    </div>
+  )
 };
 
 export default SeasonDisplay;
