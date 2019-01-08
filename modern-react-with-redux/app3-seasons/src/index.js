8eec6b18d474bca89a01cbb2ad050b921fb3f746
Fabio Pontes <faabiopontes@gmail.com>
2019-01-08 01:24:50 -0200

    FEA MRwR S6L68: Avoiding Conditionals in Render - If we have conditional logic in the render method, we should use a helper function - Helper functions on the render makes us handle change better - Helper function can make we remove duplicated code

--- modern-react-with-redux/app3-seasons/src/index.js
+++ modern-react-with-redux/app3-seasons/src/index.js
@@ -1,22 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import SeasonDisplay from './SeasonDisplay';
-import Spinner from './Spinner';
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
   state = { lat: null, long: null, errorMessage: "" };
 
-  // React says we have to define render!!
-  render() {
-    console.log("My component render function was called");
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />
+      return <SeasonDisplay lat={this.state.lat} />;
     }
     return <Spinner message="Please accept location request" />;
+  }
+
+  // React says we have to define render!!
+  render() {
+    console.log("My component render function was called");
+    return (
+      <div style={{ border: "10px solid black" }}>{this.renderContent()}</div>
+    );
   }
 
   componentDidMount() {
