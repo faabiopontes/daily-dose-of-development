2796668831078bb14c2963808dae8245f942a2fb
Fabio Pontes <faabiopontes@gmail.com>
2019-01-09 05:46:23 -0200

    FEA MRwR S7L77: Creating Event Handlers

--- modern-react-with-redux/app4-pics/README.md
+++ modern-react-with-redux/app4-pics/README.md
@@ -4,3 +4,8 @@
 - Need to get a search from the user
 - Need to use that search to make a request to an outside API and fetch data
 - Need to take the fetched images and show them on the screen in a list
+
+## Basic Event Handlers
+- onClick: If user clicks on something
+- onChange: User changes text in an input
+- onSubmit: User submits a form
--- modern-react-with-redux/app4-pics/src/components/SearchBar.js
+++ modern-react-with-redux/app4-pics/src/components/SearchBar.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <label>Image Search</label>
-          <input className="field" type="text" />
+          <input className="field" type="text" onChange={this.onInputChange} />
         </form>
       </div>
     );
