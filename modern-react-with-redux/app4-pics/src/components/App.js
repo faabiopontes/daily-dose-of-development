a8194910f8fb0f5e609f3fc9f7dc67f0e2479519
Fabio Pontes <faabiopontes@gmail.com>
2019-01-12 09:13:37 -0200

    FEA MRwR S8L89: Viewing Request Results

--- modern-react-with-redux/app4-pics/src/components/App.js
+++ modern-react-with-redux/app4-pics/src/components/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
-import SearchBar from './SearchBar';
+import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
     console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID a89b149c83299372d4e87e31219ad561fef12ebd12f07e7a1b3660910e9cfa1f"
+      }
+    });
   }
 
   render() {
     return (
-      <div className="ui container" style={{ marginTop: '10px' }}>
+      <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
     );
