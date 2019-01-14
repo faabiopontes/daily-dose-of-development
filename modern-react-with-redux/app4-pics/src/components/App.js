c6ee475ffb83c39466a6c6a0f9ce3f562eb2f3ba
Fabio Pontes <fabio.pontes@acasadoconcurseiro.com.br>
2019-01-14 14:40:50 -0200

    FEA MRwR S9L95: Rendering Lists

--- modern-react-with-redux/app4-pics/src/components/App.js
+++ modern-react-with-redux/app4-pics/src/components/App.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React from 'react';
 
-import SearchBar from "./SearchBar";
-import unsplash from "../api/unsplash";
+import SearchBar from './SearchBar';
+import unsplash from '../api/unsplash';
+import ImageList from './ImageList';
 
 class App extends React.Component {
   state = { images: [] };
@@ -29,7 +30,7 @@ class App extends React.Component {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        Found: {this.state.images.length} images
+        <ImageList images={this.state.images} />
       </div>
     );
   }
---
+++ modern-react-with-redux/app4-pics/src/components/ImageList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Image = props => {
+  console.log(props.images);
+
+  return <div>ImageList</div>;
+}
+
+export default Image;
