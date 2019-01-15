b31fe57e67d95f9ae3fe713953ae466cf3181150
Fabio Pontes <fabio.pontes@acasadoconcurseiro.com.br>
2019-01-15 11:21:02 -0200

    FEA MRwR S10L99: Grid CSS

--- modern-react-with-redux/app4-pics/src/components/ImageList.js
+++ modern-react-with-redux/app4-pics/src/components/ImageList.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import './ImageList.css';
+
 const ImageList = props => {
   console.log(props.images);
   const images = props.images.map(({ id, urls, description }) => <img key={id} src={urls.regular} alt={description} />);
 
-  return <div>{images}</div>;
+  return <div class="image-list">{images}</div>;
 }
 
 export default ImageList;
