a32fec30d6f5cfe93a9833c7f893e4772aa4b6f7
Fabio Pontes <fabio.pontes@acasadoconcurseiro.com.br>
2019-01-23 15:40:47 -0200

    FEA MRwR S11L113: Searching for Videos

--- modern-react-with-redux/app5-videos/src/apis/youtube.js
+++ modern-react-with-redux/app5-videos/src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyCqoOd9oVS_afMA0z2drDgeoyUhzuSwrOE';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY
+  },
+});
