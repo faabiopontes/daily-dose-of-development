97700c8200147d12539953cc8faaf65bc2403511
Fabio Pontes <faabiopontes@gmail.com>
2019-01-26 19:13:33 -0200

    FEA MRwR S11L115: Updating State with Fetched Data

--- modern-react-with-redux/app5-videos/src/components/App.js
+++ modern-react-with-redux/app5-videos/src/components/App.js
@@ -1,23 +1,29 @@
-import React from 'react';
-import SearchBar from './SearchBar';
+import React from "react";
+import SearchBar from "./SearchBar";
 
-import youtube from '../apis/youtube';
+import youtube from "../apis/youtube";
 
 class App extends React.Component {
-  onTermSubmit = term => {
-    console.log("onTermSubmit");
-    console.log(term);
-    youtube.get('/search', {
+  state = { videos: [] };
+  onTermSubmit = async term => {
+    const response = await youtube.get("/search", {
       params: {
         q: term
       }
     });
+
+    this.setState({ videos: response.data.items });
+
+    console.log(response);
   };
 
   render() {
-    return <div className="ui container">
-      <SearchBar onFormSubmit={this.onTermSubmit} />
-    </div>;
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        I have {this.state.videos.length} videos.
+      </div>
+    );
   }
 }
 
