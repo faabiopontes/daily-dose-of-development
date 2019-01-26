49ef7e03506b9960f5a1c0db17be0d3dfac904ad
Fabio Pontes <faabiopontes@gmail.com>
2019-01-26 19:36:45 -0200

    FEA MRwR S11L116: Passing State as Props

--- modern-react-with-redux/app5-videos/src/apis/youtube.js
+++ modern-react-with-redux/app5-videos/src/apis/youtube.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
-const KEY = 'AIzaSyCzdSi91eQVb2EkPfLcHqHI6_MdXnaD-jk';
+const KEY = "AIzaSyCzdSi91eQVb2EkPfLcHqHI6_MdXnaD-jk";
 
 export default axios.create({
-  baseURL: 'https://www.googleapis.com/youtube/v3',
+  baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
-    part: 'snippet',
+    part: "snippet",
     maxResults: 5,
     key: KEY
-  },
+  }
 });
