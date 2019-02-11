76426e914532b237255fe61e0b9f9ce4a4df0d92
Fabio Pontes <faabiopontes@gmail.com>
2019-02-11 21:28:36 -0200

    FEA MRwR S11L23: Styling the VideoDetail S11L123

--- modern-react-with-redux/app5-videos/src/components/VideoItem.js
+++ modern-react-with-redux/app5-videos/src/components/VideoItem.js
@@ -4,7 +4,11 @@ import React from "react";
 const VideoItem = ({ video, onVideoSelect }) => {
   return (
     <div className="video-item item" onClick={() => onVideoSelect(video)}>
-      <img className="ui image" alt={video.snippet.description} src={video.snippet.thumbnails.medium.url} />
+      <img
+        className="ui image"
+        alt={video.snippet.description}
+        src={video.snippet.thumbnails.medium.url}
+      />
       <div className="content">
         <div className="header">{video.snippet.title}</div>
       </div>
