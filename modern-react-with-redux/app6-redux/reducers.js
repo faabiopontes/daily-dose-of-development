0367632054826709afa2b90b6fbed61f6973b0a2
Fabio Pontes <faabiopontes@gmail.com>
2019-03-19 23:35:53 -0300

    MRwR S12L134: Rules of Reducers

--- modern-react-with-redux/app6-redux/reducers.js
+++ modern-react-with-redux/app6-redux/reducers.js
@@ -1,6 +1,6 @@
 // Reducers (Departments!)
 const claimsHistory = (oldListOfClaims = [], action) => {
-  if (action.type === 'CREATE_CLAIM') {
+  if (action.type === "CREATE_CLAIM") {
     // we care about this action (FORM!)
     return [...oldListOfClaims, action.payload];
   }
@@ -11,11 +11,21 @@ const claimsHistory = (oldListOfClaims = [], action) => {
 };
 
 const accounting = (bagOfMoney = 100, action) => {
-  if(action.type === 'CREATE_CLAIM') {
+  if (action.type === "CREATE_CLAIM") {
     return bagOfMoney - action.payload.amountOfMoneyToCollect;
-  } else if (action.type === 'CREATE_POLICY') {
+  } else if (action.type === "CREATE_POLICY") {
     return bagOfMoney - action.payload.amountOfMoneyToCollect;
   }
 
   return bagOfMoney;
-}
+};
+
+const policies = (listOfPolicies = [], action) => {
+  if (action.type === "CREATE_POLICY") {
+    return [...listOfPolicies, action.payload.name];
+  } else if (action.type === "DELETE_POLICY") {
+    return listOfPolicies.filter(name => name !== action.payload.name);
+  }
+
+  return listOfPolicies;
+};
