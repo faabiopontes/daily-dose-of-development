ef90969294508e03c3b1f0755fa32161b74cac06
Fabio Pontes <faabiopontes@gmail.com>
2019-03-26 23:20:58 -0300

    Building Web Apps with Node and Express: 15 - Serving index - Finally we were able to execute a JS with VS Code via Programs

---
+++ building-web-apps-nodejs-express/library/app.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var chalk = require("chalk");
+var debug = require("debug")("app");
+var morgan = require("morgan");
+var path = require("path");
+
+var app = express();
+
+app.use(morgan("tiny"));
+app.get("/", function(req, res) {
+  // res.send('Hello from my library app');
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.listen(3000, function() {
+  // Command to run with debug
+  // DEBUG=app node app.js
+  debug(`Listening on port ${chalk.green("3000")}`);
+});
