e3cdd3c3f1b7bbdf8f97b14c2de0c3351d98983b
Fabio Pontes <faabiopontes@gmail.com>
2019-03-27 00:55:08 -0300

    BWANE 27: Refactoring Tips in VS Code - ESLint now working with extensions - Configs uploaded

--- building-web-apps-nodejs-express/library/.eslintrc.js
+++ building-web-apps-nodejs-express/library/.eslintrc.js
@@ -1,17 +1,16 @@
 module.exports = {
     "env": {
-        "browser": true,
-        "commonjs": true,
         "es6": true,
         "node": true
     },
-    "extends": "eslint:recommended",
+    "extends": "airbnb-base",
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
     },
     "parserOptions": {
-        "ecmaVersion": 2018
+        "ecmaVersion": 2018,
+        "sourceType": "module"
     },
     "rules": {
     }
--- building-web-apps-nodejs-express/library/app.js
+++ building-web-apps-nodejs-express/library/app.js
@@ -1,24 +1,33 @@
-var express = require("express");
-var chalk = require("chalk");
-var debug = require("debug")("app");
-var morgan = require("morgan");
-var path = require("path");
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-var app = express();
+const app = express();
 
-app.use(morgan("tiny"));
-// app.use(express.static(path.join(__dirname, "/public/")));
-app.use('/css', express.static(path.join(__dirname,'/node_modules/bootstrap/dist/css')));
-app.use('/js', express.static(path.join(__dirname,'/node_modules/bootstrap/dist/js')));
-app.use('/js', express.static(path.join(__dirname,'/node_modules/jquery/dist')));
+app.use(morgan('tiny'));
+// app.use(express.static(path.join(__dirname, '/public/')));
+app.use(
+  '/css',
+  express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')),
+);
+app.use(
+  '/js',
+  express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')),
+);
+app.use(
+  '/js',
+  express.static(path.join(__dirname, '/node_modules/jquery/dist')),
+);
 
-app.get("/", function(req, res) {
+app.get('/', (req, res) => {
   // res.send('Hello from my library app');
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-app.listen(3000, function() {
+app.listen(3000, () => {
   // Command to run with debug
   // DEBUG=app node app.js
-  debug(`Listening on port ${chalk.green("3000")}`);
+  debug(`Listening on port ${chalk.green('3000')}`);
 });
