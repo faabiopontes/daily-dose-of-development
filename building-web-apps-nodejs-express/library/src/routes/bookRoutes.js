f771c351b9460832b01f0d1b4363aa694277e0e8
Fabio Pontes <faabiopontes@gmail.com>
2019-04-14 01:34:37 -0300

    BWANE: 55 - Select Many

--- building-web-apps-nodejs-express/library/src/routes/bookRoutes.js
+++ building-web-apps-nodejs-express/library/src/routes/bookRoutes.js
@@ -1,29 +1,54 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
 
 const bookRouter = express.Router();
-const sql = require('mssql');
+
+// MSSQL way
+// const sql = require('mssql');
+
 const debug = require('debug')('app:bookRoutes');
 
 function router(nav) {
-  const request = new sql.Request();
+  // MSSQL way
+  // const request = new sql.Request();
 
   bookRouter.route('/').get(async (req, res) => {
-    const { recordset } = await request.query('select * from books');
-    debug(recordset);
-    res.render('bookListView', {
-      title: 'MyLibrary',
-      books: recordset,
-      nav,
-    });
+    // MSSQL way
+    // const { recordset } = await request.query('select * from books');
+
+    // MongoDB way
+    const url = 'mongodb://localhost:27017';
+    const dbName = 'libraryApp';
+    let client;
+    try {
+      client = await MongoClient.connect(url, { useNewUrlParser: true });
+      debug('Connected correctly to server');
+
+      const db = client.db(dbName);
+      const col = await db.collection('books');
+      const books = await col.find().toArray();
+
+      debug(books);
+      res.render('bookListView', {
+        title: 'MyLibrary',
+        books,
+        nav,
+      });
+    } catch (err) {
+      debug('Error connecting to the server', err);
+    }
   });
   bookRouter
     .route('/:id')
     .all(async (req, res, next) => {
       const { id } = req.params;
-      const { recordset } = await request
-        .input('id', sql.Int, id)
-        .query('select * from books where id = @id');
-      [req.book] = recordset;
+
+      // MSSQL way
+      // const { recordset } = await request
+      //   .input('id', sql.Int, id)
+      //   .query('select * from books where id = @id');
+      // [req.book] = recordset;
+
       next();
     })
     .get((req, res) => {
