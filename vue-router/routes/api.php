6db21ef0a507dfbfaa578fa540f20d1ffff14c92
Fabio Pontes <faabiopontes@gmail.com>
2019-06-06 00:47:55 -0300

    Vue-Laravel - part 3: Users Controller & Resource

--- vue-router/routes/api.php
+++ vue-router/routes/api.php
@@ -17,14 +17,22 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Vue SPA applications are stateless
-// The API routes don't use session state
-// Meaning our application is truly stateless
-Route::get('/users', function () {
-    if (rand(1, 10) < 3) {
-        abort(500, 'We could not retrieve the users');
-    }
-    // Our factory creates Eloquent models
-    // The make() method returns a collection
-    return factory('App\User', 10)->make();
+// Telling Laravel that all Controllers used within
+// Are going to be inside the Api namespace
+// Like our UsersController
+// That is inside the namespace App\Http\Controllers\Api
+Route::namespace('Api')->group(function () {
+    Route::get('/users', 'UsersController@index');
+
+    // Vue SPA applications are stateless
+    // The API routes don't use session state
+    // Meaning our application is truly stateless
+    Route::get('/random/users', function () {
+        if (rand(1, 10) < 3) {
+            abort(500, 'We could not retrieve the users');
+        }
+        // Our factory creates Eloquent models
+        // The make() method returns a collection
+        return factory('App\User', 10)->make();
+    });
 });
