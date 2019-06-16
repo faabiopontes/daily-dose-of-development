647c0b1373aa1192ff7bf370c79e40b4d8a42607
Fabio Pontes <faabiopontes@gmail.com>
2019-06-16 18:14:30 -0300

    Vue-Laravel - part 4: Updating the User - For now we only have the Front-End

--- vue-router/resources/js/views/UsersEdit.vue
+++ vue-router/resources/js/views/UsersEdit.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <div v-if="message" class="alert">{{ message }}</div>
     <div v-if="!loaded">Loading...</div>
     <form v-else @submit.prevent="onSubmit($event)">
       <div class="form-group">
@@ -11,7 +12,7 @@
         <input id="user_email" v-model="user.email" type="email" />
       </div>
       <div class="form-group">
-        <button type="submit">Update</button>
+        <button type="submit" :disabled="saving">Update</button>
       </div>
     </form>
   </div>
@@ -22,7 +23,9 @@ import api from "../api/users";
 export default {
   data() {
     return {
+      message: null,
       loaded: false,
+      saving: false,
       user: {
         id: null,
         name: "",
@@ -36,9 +39,43 @@ export default {
     this.user = response.data.data;
   },
   methods: {
-    // onSubmit(event) {
-    // @todo form submit event
-    // }
+    async showMessage(message, miliseconds = 2000) {
+      this.message = message;
+      setTimeout(() => (this.message = null), miliseconds);
+    },
+    async onSubmit() {
+      this.saving = true;
+
+      const updatedUserData = {
+        name: this.user.name,
+        email: this.user.email
+      };
+      try {
+        const response = await api.update(this.user.id, updatedUserData);
+        this.user = response.data.data;
+        this.showMessage("User Updated!");
+      } catch (error) {
+        console.log(error);
+      } finally {
+        this.saving = false;
+      }
+    }
   }
 };
 </script>
+<style lang="scss" scoped>
+$red: lighten(red, 30%);
+$darkRed: darken($red, 50%);
+.form-group label {
+  display: block;
+}
+.alert {
+  background: $red;
+  color: $darkRed;
+  padding: 1rem;
+  margin-bottom: 1rem;
+  width: 50%;
+  border: 1px solid $darkRed;
+  border-radius: 5px;
+}
+</style>
