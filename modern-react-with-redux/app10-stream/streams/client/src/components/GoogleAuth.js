41a5c94b66e0203366e2120f50ad2b6fad2980c8
Fabio Pontes <faabiopontes@gmail.com>
2019-09-21 23:49:18 -0300

    MRwR: 212 - Rendering Authentication Status

--- modern-react-with-redux/app10-stream/streams/client/src/components/GoogleAuth.js
+++ modern-react-with-redux/app10-stream/streams/client/src/components/GoogleAuth.js
@@ -1,18 +1,35 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
-      window.gapi.client.init({
-        clientId:
-          '1004168609684-bivbksi3eh1c82gnssg0ak2fiqlv0rlf.apps.googleusercontent.com',
-        scope: 'email',
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            '1004168609684-bivbksi3eh1c82gnssg0ak2fiqlv0rlf.apps.googleusercontent.com',
+          scope: 'email',
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
     });
   }
 
   render() {
-    return <div>GoogleAuth</div>;
+    return <div>{this.renderAuthButton()}</div>;
+  }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I don't know if we are signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in</div>;
+    } else {
+      return <div>I am not signed in</div>;
+    }
   }
 }
 
