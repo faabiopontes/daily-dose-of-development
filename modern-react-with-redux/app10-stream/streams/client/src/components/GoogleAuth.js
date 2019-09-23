cc3f7c1e3b75c172e3374812595a229c8b79593b
Fabio Pontes <faabiopontes@gmail.com>
2019-09-22 22:43:02 -0300

    MRwR: 213 - Updating Auth State

--- modern-react-with-redux/app10-stream/streams/client/src/components/GoogleAuth.js
+++ modern-react-with-redux/app10-stream/streams/client/src/components/GoogleAuth.js
@@ -14,6 +14,7 @@ class GoogleAuth extends React.Component {
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
           this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
   }
@@ -21,6 +22,11 @@ class GoogleAuth extends React.Component {
   render() {
     return <div>{this.renderAuthButton()}</div>;
   }
+
+  onAuthChange = () => {
+    console.log('onAuthChange');
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
 
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
