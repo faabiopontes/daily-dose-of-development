0e2581c41d3925f1baf1ac75e10edadf27978778
Fabio Pontes <faabiopontes@gmail.com>
2019-09-24 23:42:44 -0300

    MRwR: 217: Redux Setup

--- modern-react-with-redux/app10-stream/streams/client/src/index.js
+++ modern-react-with-redux/app10-stream/streams/client/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
+// Different Routes Types
 // import { RouterTypes } from './components/RouterTypes';
 // ReactDOM.render(<RouterTypes />, document.querySelector('#root'));
 
 import App from './components/App';
-ReactDOM.render(<App />, document.querySelector('#root'));
+import reducers from './reducers';
+
+const store = createStore(reducers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root'),
+);
