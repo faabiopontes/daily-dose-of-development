212be0d3306d0a67310a25ce31350462ce7233e2
Fabio Pontes <faabiopontes@gmail.com>
2019-09-25 23:22:46 -0300

    MRwR: 218 - Connecting Auth with Action Creators

--- modern-react-with-redux/app10-stream/streams/client/src/components/GoogleAuth.js
+++ modern-react-with-redux/app10-stream/streams/client/src/components/GoogleAuth.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { signIn, signOut } from '../actions';
 
 class GoogleAuth extends React.Component {
   state = { isSignedIn: null };
@@ -23,9 +25,12 @@ class GoogleAuth extends React.Component {
     return <div>{this.renderAuthButton()}</div>;
   }
 
-  onAuthChange = () => {
-    console.log('onAuthChange');
-    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  onAuthChange = (isSignedIn) => {
+    if(isSignedIn) {
+      this.props.signIn();
+    } else {
+      this.props.signOut();
+    }
   };
 
   onSignInClick = () => {
@@ -57,4 +62,4 @@ class GoogleAuth extends React.Component {
   }
 }
 
-export default GoogleAuth;
+export default connect(null, { signIn, signOut })(GoogleAuth);
