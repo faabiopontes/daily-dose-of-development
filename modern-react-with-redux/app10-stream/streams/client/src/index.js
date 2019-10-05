4aff54dfdbfd1425637a6886a9cb5af94f4ae7bc
Fabio Pontes <faabiopontes@gmail.com>
2019-10-04 23:26:37 -0300

    MRwR: 223 - Using Redux Dev Tools to Inspect the Store - Added integration with Redux Dev Tools extension for Chrome/Firefox

--- modern-react-with-redux/app10-stream/streams/client/src/index.js
+++ modern-react-with-redux/app10-stream/streams/client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 // Different Routes Types
 // import { RouterTypes } from './components/RouterTypes';
@@ -10,7 +10,8 @@ import { createStore } from 'redux';
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
 ReactDOM.render(
   <Provider store={store}>
