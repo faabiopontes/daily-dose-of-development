ea9c185ed087ae0841b2b8702156c629a0b6019f
Fabio Pontes <faabiopontes@gmail.com>
2019-10-31 22:35:18 -0300

    MRwR: 252 - Conditionally Showing Edit and Delete

--- modern-react-with-redux/app10-stream/streams/client/src/components/streams/StreamList.js
+++ modern-react-with-redux/app10-stream/streams/client/src/components/streams/StreamList.js
@@ -7,10 +7,22 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className='right floated content'>
+          <button className='ui button primary'>Edit</button>
+          <button className='ui button negative'>Delete</button>
+        </div>
+      );
+    }
+  }
+
   renderList() {
     return this.props.streams.map(stream => {
       return (
         <div className='item' key={stream.id}>
+          {this.renderAdmin(stream)}
           <i className='large middle aligned icon camera' />
           <div className='content'>
             {stream.title}
@@ -35,9 +47,10 @@ const mapDispatchToProps = {
   fetchStreams,
 };
 
-const mapStateToProps = ({ streams }) => {
+const mapStateToProps = state => {
   return {
-    streams: Object.values(streams),
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
   };
 };
 
