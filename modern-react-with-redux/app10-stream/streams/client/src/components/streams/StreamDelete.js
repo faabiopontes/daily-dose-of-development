796ed963ccbc7fa62ac5db0c413820f8b7cda3c0
Fabio Pontes <faabiopontes@gmail.com>
2019-11-20 23:42:02 -0300

    MRwR: 277 - OnDismiss From the Parent - Added Tasks for automatic ease of start

--- modern-react-with-redux/app10-stream/streams/client/src/components/streams/StreamDelete.js
+++ modern-react-with-redux/app10-stream/streams/client/src/components/streams/StreamDelete.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Modal from '../Modal';
+import history from '../../history';
 
 const StreamDelete = () => {
   const actions = (
@@ -14,8 +15,9 @@ const StreamDelete = () => {
       <div>StreamDelete</div>
       <Modal
         title='Delete Stream'
-        content='Are you sure you want to delete this stream?'
+        content='Are you sre you want to delete this stream?'
         actions={actions}
+        onDismiss={() => history.push('/')}
       />
     </div>
   );
