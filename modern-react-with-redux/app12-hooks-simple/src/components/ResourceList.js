8748a649eaf35621c1aca002e2d21dfdd10f9ddb
Fabio Pontes <faabiopontes@gmail.com>
2020-02-23 12:00:56 -0300

    MRwR: 331 - Code Reuse with Hooks

--- modern-react-with-redux/app12-hooks-simple/src/components/ResourceList.js
+++ modern-react-with-redux/app12-hooks-simple/src/components/ResourceList.js
@@ -1,26 +1,16 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-
-const useResources = resource => {
-  const [resources, setResources] = useState([]);
-
-  useEffect(() => {
-    (async function fetchResource(resource) {
-      const response = await axios.get(
-        `http://jsonplaceholder.typicode.com/${resource}`
-      );
-
-      setResources(response.data);
-    })(resource);
-  }, [resource]);
-
-  return resources;
-};
+import React from "react";
+import useResources from "./useResources";
 
 const ResourceList = ({ resource }) => {
   const resources = useResources(resource);
 
-  return <ul>{resources.map(record => <li key={record.id}>{record.title}</li>)}</ul>;
+  return (
+    <ul>
+      {resources.map(record => (
+        <li key={record.id}>{record.title}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default ResourceList;
