74ab94d9755f318d189e27e24556b0d72a570410
Fabio Pontes <faabiopontes@gmail.com>
2020-06-18 22:06:30 -0300

    GS: Middlewares - Using multiple middlewares for the same route - Using middlewares in different ways

--- go-stack/dev-concepts/backend/src/index.js
+++ go-stack/dev-concepts/backend/src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { uuid } = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
@@ -22,7 +22,35 @@ app.use(express.json());
  * Request Body: Content of resource to Create or Update (JSON)
  */
 
+/**
+ * Middlware: Request interceptor
+ *
+ * Stop requests or change its data
+ */
+
 const projects = [];
+
+const logRequests = (request, response, next) => {
+  const { method, url } = request;
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.time(logLabel);
+  next();
+  console.timeEnd(logLabel);
+};
+
+const validateProjectId = (request, response, next) => {
+  const { id } = request.params;
+
+  if(!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalid project ID.' });
+  }
+
+  next();
+};
+
+app.use(logRequests);
+app.use('/projects/:id', validateProjectId);
 
 app.get('/projects', (request, response) => {
   // Query Params
