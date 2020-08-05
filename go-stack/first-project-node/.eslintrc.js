5b7dbabd17551eebbd86c8708d8947a5325db3da
Fabio Pontes <faabiopontes@gmail.com>
2020-08-05 13:55:57 -0300

    GS: First Node Project: Project Patterns - Eslint with typescript-lint - Editor Config - Prettier

---
+++ go-stack/first-project-node/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'prettier',
+  ],
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': 'error'
+  }
+};
