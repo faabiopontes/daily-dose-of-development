eb0a4c880f979c6161debb9ca648b6b5c2306fe7
Fabio Pontes <faabiopontes@gmail.com>
2020-08-11 20:25:57 -0300

    GS: Starting BackEnd: Password Cryptography

--- go-stack/first-project-node/src/routes/users.routes.ts
+++ go-stack/first-project-node/src/routes/users.routes.ts
@@ -8,6 +8,7 @@ usersRouter.post('/', async (request, response) => {
     const { name, email, password } = request.body;
     const createUserService = new CreateUserService();
     const user = await createUserService.execute({ name, email, password });
+    delete user.password;
 
     return response.json(user);
   } catch (err) {
