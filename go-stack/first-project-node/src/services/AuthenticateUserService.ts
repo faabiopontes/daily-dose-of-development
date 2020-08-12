2b7dcbe0e916fa35ef1767b462f5338a98940a48
Fabio Pontes <faabiopontes@gmail.com>
2020-08-12 01:39:37 -0300

    GS: Starting BackEnd: Generating JWT

--- go-stack/first-project-node/src/services/AuthenticateUserService.ts
+++ go-stack/first-project-node/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import User from '../models/User';
 
 interface Request {
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -28,7 +30,12 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
-    return { user };
+    const token = sign({}, '48d1d00b4be5ead188959d753cc0dfdc', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
   }
 }
 
