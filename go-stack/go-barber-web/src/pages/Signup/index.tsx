9c443c8ce80bdc1a48138a89c8193b1af1b98fde
Fabio Pontes <faabiopontes@gmail.com>
2020-09-13 00:07:20 -0300

    GS: Validating SignUp - yup package added

--- go-stack/go-barber-web/src/pages/Signup/index.tsx
+++ go-stack/go-barber-web/src/pages/Signup/index.tsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiLogIn, FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
+
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
 
 import Input from '../../components/input';
 import Button from '../../components/button';
 
+interface FormInputs {
+  name?: string;
+  email?: string;
+  password?: string;
+}
+
 const Signup: React.FC = () => {
-  const handleSubmit = (data: object): void => {
-    console.log(data);
-  };
+  const handleSubmit = useCallback(async (data: FormInputs): Promise<void> => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No mínimo 6 digitos'),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
