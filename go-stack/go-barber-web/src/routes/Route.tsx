1820914d078bde877915d5a8ca911b6b7aaa48a5
Fabio Pontes <faabiopontes@gmail.com>
2020-09-19 20:21:18 -0300

    GS: Starting Front-End: Authenticated Routes

---
+++ go-stack/go-barber-web/src/routes/Route.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  Redirect,
+  Route as ReactDOMRoute,
+  RouteProps as ReactDOMRouteProps,
+} from 'react-router-dom';
+import { useAuth } from '../hooks/auth';
+
+interface IRouteProps extends ReactDOMRouteProps {
+  isPrivate?: boolean;
+  component: React.ComponentType;
+}
+
+const Route: React.FC<IRouteProps> = ({
+  isPrivate = false,
+  component: Component,
+  ...rest
+}) => {
+  const { user } = useAuth();
+  const isSigned = Boolean(user);
+
+  return (
+    <ReactDOMRoute
+      {...rest}
+      render={({ location }) => {
+        if (isPrivate === isSigned) {
+          return <Component />;
+        }
+
+        return (
+          <Redirect
+            to={{
+              pathname: isPrivate ? '/' : '/bashboard',
+              state: { from: location },
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export default Route;
--- go-stack/go-barber-web/src/routes/index.tsx
+++ go-stack/go-barber-web/src/routes/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
 import Signin from '../pages/Signin';
 import Signup from '../pages/Signup';
+import Route from './Route';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Signin} />
     <Route path="/signup" exact component={Signup} />
+    <Route path="/dashboard" exact component={Dashboard} isPrivate />
   </Switch>
 );
 
