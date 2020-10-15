de9fe3ad426e969eb543e433f5db821a1989d592
Fabio Pontes <faabiopontes@gmail.com>
2020-10-14 23:37:10 -0300

    GS: Architecture and Tests on Node.js: Separated in modules

--- go-stack/first-project-node/src/modules/appointments/services/CreateAppointmentService.ts
+++ go-stack/first-project-node/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,7 +1,7 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 
-import Appointment from '../models/Appointment';
+import Appointment from '../../appointments/entities/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 interface Request {
