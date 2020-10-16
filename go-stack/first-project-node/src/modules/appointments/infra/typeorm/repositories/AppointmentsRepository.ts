0539485dc1a5fe5c57df565d7f84c104ea16cabb
Fabio Pontes <faabiopontes@gmail.com>
2020-10-15 23:39:16 -0300

    GS: Architecture and Tests in Node.js: Rewriting Repositories

--- go-stack/first-project-node/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ go-stack/first-project-node/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,16 +1,32 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { getRepository, Repository } from 'typeorm';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/CreateAppointmentDTO';
 import Appointment from '../entities/Appointment';
 
-@EntityRepository(Appointment)
-class AppointmentsRepository extends Repository<Appointment>
-  implements IAppointmentsRepository {
+class AppointmentsRepository implements IAppointmentsRepository {
+  private ormRepository: Repository<Appointment>;
+
+  constructor() {
+    this.ormRepository = getRepository(Appointment);
+  }
+
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const foundAppointment = await this.findOne({
+    const foundAppointment = await this.ormRepository.findOne({
       where: { date },
     });
 
     return foundAppointment;
+  }
+
+  public async create({
+    provider_id,
+    date,
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = this.ormRepository.create({ provider_id, date });
+
+    await this.ormRepository.save(appointment);
+
+    return appointment;
   }
 }
 
--- go-stack/first-project-node/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ go-stack/first-project-node/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,5 +1,7 @@
+import ICreateAppointmentDTO from '../dtos/CreateAppointmentDTO';
 import Appointment from '../entities/Appointment';
 
 export default interface IAppointmentsRepository {
+  create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
 }
