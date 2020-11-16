1a934919698ea3ed704fd62996cd99820f056a98
Fabio Pontes <faabiopontes@gmail.com>
2020-11-15 21:46:04 -0300

    GS: Test Environment: Creating first test from zero

---
+++ go-stack/go-barber-web/src/__tests__/pages/Signin.spec.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import SignIn from '../../pages/Signin';
+
+jest.mock('react-router-dom', () => {
+  return {
+    Link: ({ children }: { children: React.ReactNode }) => children,
+    useHistory: () => ({ push: jest.fn() }),
+  };
+});
+
+describe('SignIn Page', () => {
+  it('should be able to sign in', () => {
+    const { debug } = render(<SignIn />);
+
+    debug();
+  });
+});
