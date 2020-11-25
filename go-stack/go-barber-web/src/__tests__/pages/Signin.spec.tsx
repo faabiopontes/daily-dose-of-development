56cf47759e9566546d53d2bde8a692ff81c41104
Fabio Pontes <faabiopontes@gmail.com>
2020-11-25 08:41:18 -0300

    GS: Test Environment: Mock Hook Authentication - Updated `react-scripts` to `4.0.1` - Updated `eslint` to `^7.11.0` - Updated snapshot since the generated one had different classes

--- go-stack/go-barber-web/src/__tests__/pages/Signin.spec.tsx
+++ go-stack/go-barber-web/src/__tests__/pages/Signin.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import SignIn from '../../pages/Signin';
 
 const mockedHistoryPush = jest.fn();
@@ -13,8 +13,20 @@ jest.mock('react-router-dom', () => {
   };
 });
 
+jest.mock('../../hooks/auth', () => ({
+  useAuth: () => ({
+    signIn: jest.fn(),
+  }),
+}));
+
+jest.mock('../../hooks/toast', () => ({
+  useToast: () => ({
+    addToast: jest.fn(),
+  }),
+}));
+
 describe('SignIn Page', () => {
-  it('should be able to sign in', () => {
+  it('should be able to sign in', async () => {
     const { getByPlaceholderText, getByText } = render(<SignIn />);
 
     const emailField = getByPlaceholderText('E-mail');
@@ -30,7 +42,7 @@ describe('SignIn Page', () => {
 
     fireEvent.click(buttonElement);
 
-    expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard');
+    await waitFor(() => expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard'));
   });
 
   it('should be rendered', () => {
