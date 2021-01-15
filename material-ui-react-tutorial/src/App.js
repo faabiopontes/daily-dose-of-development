b9913d573efbdf3d9e78de84c687c3ead3aff183
Fabio Pontes <faabiopontes@gmail.com>
2021-01-15 06:52:29 -0300

    MURT: ButtonGroup, Checkbox, TextField

--- material-ui-react-tutorial/src/App.js
+++ material-ui-react-tutorial/src/App.js
@@ -1,21 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
-import { Button } from '@material-ui/core';
+import { Button, ButtonGroup, Checkbox, FormControlLabel, TextField } from '@material-ui/core';
 import SaveIcon from '@material-ui/icons/Save'
+import DeleteIcon from '@material-ui/icons/Delete';
+import { useState } from 'react';
+
+function CheckboxExample() {
+  const [checked, setChecked] = useState(true);
+
+  return (
+    <div>
+      <FormControlLabel
+        label="Testing Checkbox"
+        control={<Checkbox
+        checked={checked}
+        onChange={(e)=>setChecked(e.target.checked)}
+        color="primary"
+        // disabled
+        icon={<SaveIcon />}
+        checkedIcon={<DeleteIcon />}
+        inputProps={{
+          'aria-label': 'secondary checkbox'
+        }} />}
+      />
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button
-          startIcon={<SaveIcon />}
-          endIcon={<SaveIcon />}
-          onClick={() => alert("Hello")}
-          size="large"
-          variant="contained"
-          color="secondary">
-          Hello World
-        </Button>
+        <TextField
+          variant="outlined"
+          color="secondary"
+          label="The time"
+          type="email"
+          placeholder="test@test.com"
+        />
+        <CheckboxExample />
+        <ButtonGroup>
+          <Button
+            startIcon={<SaveIcon />}
+            onClick={() => alert("Hello")}
+            size="large"
+            variant="contained"
+            color="primary">
+            Save
+          </Button>
+          <Button
+            endIcon={<DeleteIcon />}
+            onClick={() => alert("Hello")}
+            size="large"
+            variant="contained"
+            color="secondary">
+            Discard
+          </Button>
+
+        </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
