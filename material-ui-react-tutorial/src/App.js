c46dac946644223bb8b7a3ef50465dba7e4fd97f
Fabio Pontes <faabiopontes@gmail.com>
2021-01-16 20:33:58 -0300

    MURT: makeStyles for Buttons

--- material-ui-react-tutorial/src/App.js
+++ material-ui-react-tutorial/src/App.js
@@ -1,9 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import { Button, ButtonGroup, Checkbox, FormControlLabel, TextField } from '@material-ui/core';
+import { Button, ButtonGroup, Checkbox, FormControlLabel, makeStyles, TextField } from '@material-ui/core';
 import SaveIcon from '@material-ui/icons/Save'
 import DeleteIcon from '@material-ui/icons/Delete';
 import { useState } from 'react';
+
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #333, #999)',
+    border: 0,
+    marginBottom: 15,
+    borderRadius: 15,
+    color: 'white',
+    padding: '5px 30px'
+  }
+});
+
+function ButtonStyled() {
+  const classes = useStyles();
+  return <Button className={classes.root}>Test Styled Button</Button>;
+}
 
 function CheckboxExample() {
   const [checked, setChecked] = useState(true);
@@ -32,6 +48,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Material UI React Tutorial</h1>
+        <ButtonStyled />
         <TextField
           variant="outlined"
           color="secondary"
