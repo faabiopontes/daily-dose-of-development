6e21537d09bc9442c6c46e341063f90ae2fd667a
Fabio Pontes <faabiopontes@gmail.com>
2021-03-11 07:50:25 -0300

    NZH: Validation: AuthCredentialsDto, password strength

--- nestjs-task-management/src/auth/dto/auth-credentials.dto.ts
+++ nestjs-task-management/src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,19 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
+const regexPasswordWithUpperCaseLowerCaseNumber = /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(regexPasswordWithUpperCaseLowerCaseNumber, {
+    message:
+      'password must contain at least one upper case letter, one lower case letter, and a number',
+  })
   password: string;
 }
