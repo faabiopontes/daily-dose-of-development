252798a63911bdb03814abb25dab5d98c4e3c678
Fabio Pontes <faabiopontes@gmail.com>
2021-03-26 23:16:27 -0300

    NZH: Configuration Management Set-up

--- nestjs-task-management/src/main.ts
+++ nestjs-task-management/src/main.ts
@@ -3,11 +3,16 @@ declare const module: any;
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import * as config from 'config';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const port = 3000;
   const app = await NestFactory.create(AppModule);
+
+  const serverConfig = config.get('server');
+  console.log({ serverConfig });
+
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
 
