43b81d62a114b363307873cf0ea0160ea2deb8e9
Fabio Pontes <faabiopontes@gmail.com>
2021-03-30 23:21:46 -0300

    NZH: Testing TasksService - Part 2 - getTaskById

--- nestjs-task-management/src/tasks/tasks.service.spec.ts
+++ nestjs-task-management/src/tasks/tasks.service.spec.ts
@@ -4,11 +4,19 @@ import { TaskRepository } from './task.repository';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatus } from './task-status-enum';
 import { User } from '../auth/user.entity';
+import { Task } from './task.entity';
+import { NotFoundException } from '@nestjs/common';
 
 const mockTaskRepository = () => ({
   getTasks: jest.fn(),
+  findOne: jest.fn(),
 });
-const mockUser: User = { username: 'Fabio Pontes' } as User;
+const mockUser: User = { id: 1, username: 'Fabio Pontes' } as User;
+const mockTask: Task = {
+  id: 2,
+  title: 'Test task',
+  description: 'Test desc',
+} as Task;
 
 describe('TasksService', () => {
   let tasksService: TasksService;
@@ -41,6 +49,32 @@ describe('TasksService', () => {
 
       expect(result).toEqual('someValue');
       expect(taskRepository.getTasks).toHaveBeenCalled();
+    });
+  });
+
+  describe('getTaskById', () => {
+    it('calls taskRepository.findOne() and successfully retrieve and return the task', async () => {
+      taskRepository.findOne.mockResolvedValue(mockTask);
+
+      expect(taskRepository.findOne).not.toHaveBeenCalled();
+
+      const result = await tasksService.getTaskById(mockTask.id, mockUser);
+
+      expect(result).toEqual(mockTask);
+      expect(taskRepository.findOne).toHaveBeenCalledWith({
+        id: mockTask.id,
+        user: { id: mockUser.id },
+      });
+    });
+
+    it('throws an error as task is not found', async () => {
+      taskRepository.findOne.mockResolvedValue(null);
+
+      expect(taskRepository.findOne).not.toHaveBeenCalled();
+
+      expect(tasksService.getTaskById(mockTask.id, mockUser)).rejects.toThrow(
+        NotFoundException,
+      );
     });
   });
 });
