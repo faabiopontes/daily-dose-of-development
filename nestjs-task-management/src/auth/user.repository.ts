f6be7c27647e543ab93e436494ca7c78555f6bf4
Fabio Pontes <faabiopontes@gmail.com>
2021-04-03 01:03:12 -0300

    NZH: Testing UserRepository

--- nestjs-task-management/src/auth/user.repository.ts
+++ nestjs-task-management/src/auth/user.repository.ts
@@ -12,7 +12,7 @@ export class UserRepository extends Repository<User> {
   async signUp(authCredentialsDto: AuthCredentialsDto) {
     const { username, password } = authCredentialsDto;
 
-    const user = new User();
+    const user = this.create();
     user.username = username;
     user.salt = await genSalt();
     user.password = await this.hashPassword(password, user.salt);
