98e955a83d3c102f32abd9b0b4b3b9cb784ce5b5
Fabio Pontes <faabiopontes@gmail.com>
2021-04-22 11:46:18 -0300

    ERBD: Persist List Reordering with react-beautiful-dnd using the onDragEnd Callback

---
+++ egghead-react-beautiful-dnd/src/example-result.js
@@ -0,0 +1,17 @@
+const result = {
+  combine: null,
+  destionation: {
+    droppableId: 'column-1',
+    index: 1,
+  },
+  draggableId: 'task-1',
+  mode: 'FLUID',
+  reason: 'DROP',
+  source: {
+    index: 0,
+    droppableId: 'column-1',
+  },
+  type: 'DEFAULT',
+};
+
+export default result;
--- egghead-react-beautiful-dnd/src/index.js
+++ egghead-react-beautiful-dnd/src/index.js
@@ -8,15 +8,44 @@ import { DragDropContext } from 'react-beautiful-dnd';
 class App extends React.Component {
   state = initialData;
 
-  onDragEnd = result => {
-    console.log('onDragEnd');
-    console.log({ result });
-    // TODO: reorder our column
-  }
+  onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = this.state.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds,
+    };
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newColumn.id]: newColumn,
+      },
+    };
+
+    this.setState(newState);
+  };
 
   render() {
     return (
-      <DragDropContext onDragend={this.onDragEnd}>
+      <DragDropContext onDragEnd={this.onDragEnd}>
         {this.state.columnOrder.map((columnId) => {
           const column = this.state.columns[columnId];
           const tasks = column.taskIds.map(
